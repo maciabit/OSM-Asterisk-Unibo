d139b17245980ccd4e38a43f79e96df2097ff1ce
Mattia <mattia.rossi15@studio.unibo.it>
2021-06-03 11:16:27 +0200

    clean

--- Asterisk-Unibo_vnfd/charms/native-charm/src/charm.py
+++ Asterisk-Unibo_vnfd/charms/native-charm/src/charm.py
@@ -9,66 +9,54 @@ from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus
 
-from string_utils import roman_encode
-from utils import (
-	write
-)
-
 logger = logging.getLogger(__name__)
 
-extensions_conf = '/etc/asterisk/extensions.conf'
-pjsip_conf = '/etc/asterisk/pjsip.conf'
+extensions_conf = "/etc/asterisk/extensions.conf"
+pjsip_conf = "/etc/asterisk/pjsip.conf"
+
 
 class NativeCharmCharm(CharmBase):
+    def __init__(self, *args):
+        super().__init__(*args)
 
-	def __init__(self, *args):
-		super().__init__(*args)
+        # Listen to charm events
+        self.framework.observe(self.on.config_changed, self.on_config_changed)
+        self.framework.observe(self.on.install, self.on_install)
+        self.framework.observe(self.on.start, self.on_start)
 
-		# Listen to charm events
-		self.framework.observe(self.on.config_changed, self.on_config_changed)
-		self.framework.observe(self.on.install, self.on_install)
-		self.framework.observe(self.on.start, self.on_start)
+        self.framework.observe(self.on.start_asterisk_action, self.on_start_asterisk)
+        self.framework.observe(self.on.add_user_action, self.on_add_user)
+        self.framework.observe(self.on.remove_user_action, self.on_remove_user)
 
-		self.framework.observe(self.on.writetofile_action, self.on_write_to_file)
-		self.framework.observe(self.on.start_asterisk_action, self.on_start_asterisk)
-		self.framework.observe(self.on.add_user_action, self.on_add_user)
-		self.framework.observe(self.on.remove_user_action, self.on_remove_user)
+    def on_config_changed(self, event):
+        """Handle changes in configuration"""
+        self.model.unit.status = ActiveStatus()
 
-	def on_config_changed(self, event):
-		"""Handle changes in configuration"""
-		self.model.unit.status = ActiveStatus()
+    def on_install(self, event):
+        """Called when the charm is being installed"""
+        self.model.unit.status = ActiveStatus()
 
-	def on_install(self, event):
-		"""Called when the charm is being installed"""
-		self.model.unit.status = ActiveStatus()
+    def on_start(self, event):
+        """Called when the charm is being started"""
+        self.model.unit.status = ActiveStatus()
 
-	def on_start(self, event):
-		"""Called when the charm is being started"""
-		self.model.unit.status = ActiveStatus()
+    def on_start_asterisk(self, event):
+        subprocess.run("asterisk -cvvvvv", shell=True)
 
-	# To be deleted
-	def on_write_to_file(self, event):
-		all_params = event.params
-		filename = all_params['filename']
-		write(filename)
-		logger.info("Trying the string util lib: roman encode of 37 is {}".format(roman_encode(37)))
-		event.set_results({"edited": True, "filename": filename})
+    def on_add_user(self, event):
+        all_params = event.params
+        username = all_params["username"]
+        password = all_params["password"]
 
-	def on_start_asterisk(self, event):
-		subprocess.run('asterisk -cvvvvv', shell=True)
+        # Add user to extensions.conf
+        with open(extensions_conf, "a") as f:
+            f.write(f"exten => {username},1,Dial(PJSIP/{username},20)\n")
 
-	def on_add_user(self, event):
-		all_params = event.params
-		username = all_params['username']
-		password = all_params['password']
-
-		# Add user to extensions.conf
-		with open(extensions_conf, 'a') as f:
-			f.write(f'exten => {username},1,Dial(PJSIP/{username},20)\n')
-
-		# Add user to pjsip.conf
-		with open(pjsip_conf, 'a') as f:
-			f.write(textwrap.dedent(f'''
+        # Add user to pjsip.conf
+        with open(pjsip_conf, "a") as f:
+            f.write(
+                textwrap.dedent(
+                    f"""
 				[{username}](template_hackfest)
 				auth={username}
 				aors={username}
@@ -78,38 +66,46 @@ class NativeCharmCharm(CharmBase):
 				password={password}
 
 				[{username}](aor_dynamic)
-				'''))
+				"""
+                )
+            )
 
-		subprocess.run('asterisk -rx "reload"', shell=True)
+        subprocess.run('asterisk -rx "reload"', shell=True)
 
-	def on_remove_user(self, event):
-		all_params = event.params
-		username = all_params['username']
+    def on_remove_user(self, event):
+        all_params = event.params
+        username = all_params["username"]
 
-		# Remove user from extensions.conf
-		with open(extensions_conf, 'r') as f:
-			lines = f.readlines()
-		with open(extensions_conf, 'w') as f:
-			for line in lines:
-				if line.strip('\n') != f'exten => {username},1,Dial(PJSIP/{username},20)':
-					f.write(line)
+        # Remove user from extensions.conf
+        with open(extensions_conf, "r") as f:
+            lines = f.readlines()
+        with open(extensions_conf, "w") as f:
+            for line in lines:
+                if (
+                    line.strip("\n")
+                    != f"exten => {username},1,Dial(PJSIP/{username},20)"
+                ):
+                    f.write(line)
 
-		# Remove user from pjsip.conf
-		with open(pjsip_conf, 'r') as f:
-			file_content = f.read()
-		x = re.search(
-			f'(?s)(?<=\[{username}\]\(template_hackfest\)).*?(?=\[{username}\]\(aor_dynamic\))',
-			file_content,
-			flags=re.MULTILINE
-		)
-		if not x: return
-		user_settings = f'\n[{username}](template_hackfest){x[0]}[{username}](aor_dynamic)\n'
-		file_content = file_content.replace(user_settings, '')
-		with open(pjsip_conf, 'w') as f:
-			f.write(file_content)
+        # Remove user from pjsip.conf
+        with open(pjsip_conf, "r") as f:
+            file_content = f.read()
+        x = re.search(
+            f"(?s)(?<=\[{username}\]\(template_hackfest\)).*?(?=\[{username}\]\(aor_dynamic\))",
+            file_content,
+            flags=re.MULTILINE,
+        )
+        if not x:
+            return
+        user_settings = (
+            f"\n[{username}](template_hackfest){x[0]}[{username}](aor_dynamic)\n"
+        )
+        file_content = file_content.replace(user_settings, "")
+        with open(pjsip_conf, "w") as f:
+            f.write(file_content)
 
-		subprocess.run('asterisk -rx "reload"', shell=True)
+        subprocess.run('asterisk -rx "reload"', shell=True)
 
 
 if __name__ == "__main__":
-	main(NativeCharmCharm)
+    main(NativeCharmCharm)
